3eabc01915b2609ab2c66f9e6f675db564e6aa88
sanjay <sanjay.r@roofandfloor.com>
2019-04-03 18:15:14 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState}  from 'react';
+import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import Authcontext from './components/auth-context';
 
-class App extends Component {
-  render() {
+
+const app = props => {
+const [page, setPage] = useState('auth');
+
+const [authStatus, setAuthStatus] = useState(false);
+
+const switchPage = pageName => {
+   setPage(pageName);
+  };
+
+const login = () => {
+  setAuthStatus(true);
+};
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="App">
+        <Authcontext.Provider value={{status: authStatus, login:login}}>
+          <Header
+            onLoadTodos={switchPage.bind(this, 'todos')}
+            onLoadAuth={switchPage.bind(this, 'auth')}
+           />     
+              <hr/>
+      {page === 'auth' ?  <Auth/> : <Todo/> } 
+        </Authcontext.Provider>  
+    </div>
     );
-  }
-}
+  };
 
-export default App;
+export default app;
---
+++ src/components/Todo.js
@@ -0,0 +1,79 @@
+import React, {useState ,useEffect} from 'react';
+import axios from 'axios';
+
+
+const todo = props => {
+const [todoName,setTodoName] = useState('');
+const [todoList,setTodoList] = useState([]);
+
+//const [todoState, setTodoState] = useState({userInput:'', todoList:[]})
+
+
+useEffect(() => {
+    axios.get('https://webpacks-4e54b.firebaseio.com/todo.json')
+    .then(result => {
+        console.log(result);
+        const todoData = result.data;
+        const todos = [];
+        for (const key in todoData){
+            todos.push({id:key,name:todoData[key].name})
+        }
+        setTodoList(todos);
+    });
+    return () => {
+        console.log('clean up');
+    };
+
+},[todoName]);
+
+const mouseMoveHandler = event => {
+    console.log(event.clientX,event.clientY);
+};
+
+useEffect(() => {
+document.addEventListener('mousemove',mouseMoveHandler);
+return() =>{
+    document.removeEventListener('mousemove',mouseMoveHandler);
+    };    
+});
+
+const inputchangehandler = event => {
+    setTodoName(event.target.value);
+    // setTodoState({
+    // userInput:event.target.value,
+    // todoList:todoState.todoList
+    // });
+};
+const nameChangeHandler = () => {
+    setTodoList(todoList.concat(todoName));
+//    setTodoState({
+//     userInput : todoState.todoList,
+//     todoList: todoState.todoList.concat(todoState.userInput)
+//    });
+
+        axios.post('https://webpacks-4e54b.firebaseio.com/todo.json',{name:todoName})
+        .then(res => { 
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
+
+    return(
+        <React.Fragment>
+            <input type="text" 
+            placeholder="todo"
+            onChange={inputchangehandler}
+            value={todoName}
+            //value={todoState.userInput}
+            />
+            <button type = "button" onClick={nameChangeHandler}>ADD</button>
+            <ul>{todoList.map( todo => (
+               <li key={todo.id}>{todo.name}</li> 
+            ))}</ul>
+        </React.Fragment>
+    )    
+}
+
+export default todo;
